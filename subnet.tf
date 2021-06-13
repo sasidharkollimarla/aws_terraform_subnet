db783c36a58aa720b23880d62e5cb80703b0bcfc
sasidharkollimarla <sasidharkollimarla@outlook.com>
2021-06-13 16:35:32 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,20 +1,20 @@
 
 #================ Public Subnet ================
 
-resource "aws_subnet" "public-subnet-1" {
-  vpc_id            = "${var.vpc_id}"
-  availability_zone = "${var.aws_region}a"
-  cidr_block        = "${var.cidr_block_public-subnet-1}"
-  map_public_ip_on_launch = "true"
+#resource "aws_subnet" "public-subnet-1" {
+#  vpc_id            = "${var.vpc_id}"
+#  availability_zone = "${var.aws_region}a"
+#  cidr_block        = "${var.cidr_block_public-subnet-1}"
+#  map_public_ip_on_launch = "true"
 
-  tags = {
-    Name = "public-terraform-subnet-1"
-  }
-}
+#  tags = {
+#    Name = "public-terraform-subnet-1"
+#  }
+#}
 
-output "out_public-subnet-1_id" {
-  value = "${aws_subnet.public-subnet-1.id}"
-}
+#output "out_public-subnet-1_id" {
+#  value = "${aws_subnet.public-subnet-1.id}"
+#}
 
 resource "aws_subnet" "public-subnet-2" {
   vpc_id            = "${var.vpc_id}"
