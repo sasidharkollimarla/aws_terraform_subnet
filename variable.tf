f2367a545f4bdd0fdebcc0324974beb22315a6d7
sasidharkollimarla <sasidharkollimarla@outlook.com>
2021-06-13 16:05:38 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,8 +1,18 @@
 variable "aws_region" {
   default = "ap-south-1"
 }
-variable "vpc_id" {}
-variable "cidr_block_public-subnet-1" {}
-variable "cidr_block_public-subnet-2" {}
-variable "cidr_block_private-subnet-1" {}
-variable "cidr_block_private-subnet-2" {}
+variable "vpc_id" {
+  default = 'vpc-ea1cdf81'
+}
+variable "cidr_block_public-subnet-1" {
+  default = '172.31.2.0/20'
+}
+variable "cidr_block_public-subnet-2" {
+default = '172.31.3.0/20'
+}
+variable "cidr_block_private-subnet-1" {
+default = '172.31.4.0/20'
+}
+variable "cidr_block_private-subnet-2" {
+default = '172.31.5.0/20'
+}
